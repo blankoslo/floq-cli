2c15ee3dfa24d00f2ba173d2c2189813d590e94e
Even Kallevik <even.kallevik@gmail.com>
2020-09-11 20:04:31 +0200

    Add basic commands

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,3 @@
-// Working with subcommands is simple. There are a few key points to remember when working with
-// subcommands in clap. First, they are really just Apps. This means they can have their own
-// settings, version, authors, args, and even their own subcommands. The next thing to remember is
-// that subcommands are set up in a tree like hierarchy.
-//
-// An ASCII art depiction may help explain this better. Using a fictional version of git as the demo
-// subject. Imagine the following are all subcommands of git (note, the author is aware these aren't
-// actually all subcommands in the real git interface, but it makes explanation easier)
-//
-//            Top Level App (git)                         TOP
-//                           |
-//    -----------------------------------------
-//   /             |                \          \
-// clone          push              add       commit      LEVEL 1
-//   |           /    \            /    \       |
-//  url      origin   remote    ref    name   message     LEVEL 2
-//           /                  /\
-//        path            remote  local                   LEVEL 3
-//
-// Given the above fictional subcommand hierarchy, valid runtime uses would be (not an all inclusive
-// list):
-//
-// $ git clone url
-// $ git push origin path
-// $ git add ref local
-// $ git commit message
-//
-// Notice only one command per "level" may be used. You could not, for example, do:
-//
-// $ git clone url push origin path
-//
-// It's also important to know that subcommands each have their own set of matches and may have args
-// with the same name as other subcommands in a different part of the tree hierarchy (i.e. the arg
-// names aren't in a flat namespace).
-//
-// In order to use subcommands in clap, you only need to know which subcommand you're at in your
-// tree, and which args are defined on that subcommand.
-//
-// Let's make a quick program to illustrate. We'll be using the same example as above but for
-// brevity sake we won't implement all of the subcommands, only a few.
-
 use clap::{App, AppSettings, Arg};
 
 fn main() {
@@ -47,51 +6,61 @@ fn main() {
         .version("1.0")
         .author("The Rust Gang")
         .subcommand(
-            App::new("clone")
-                .about("clones repos")
-                .arg(Arg::new("repo").about("The repo to clone").required(true)),
-        )
-        .subcommand(
-            App::new("push")
-                .about("pushes things")
-                .setting(AppSettings::SubcommandRequiredElseHelp)
-                .subcommand(
-                    App::new("remote") // Subcommands can have their own subcommands,
-                        // which in turn have their own subcommands
-                        .about("pushes remote things")
-                        .arg(
-                            Arg::new("repo")
-                                .required(true)
-                                .about("The remote repo to push things to"),
-                        ),
-                )
-                .subcommand(App::new("local").about("pushes local things")),
-        )
-        .subcommand(
-            App::new("add")
-                .about("adds things")
-                .author("Someone Else") // Subcommands can list different authors
-                .version("v2.0 (I'm versioned differently)") // or different version from their parents
-                .setting(AppSettings::ArgRequiredElseHelp) // They can even have different settings
+            App::new("projects")
+                .about("Lists name and code of projects")
                 .arg(
-                    Arg::new("stuff")
-                        .long("stuff")
-                        .about("Stuff to add")
+                    Arg::new("all")
+                        .about("Flag to list all project")
+                        .short('a')
+                        .long("all")
+                ),
+        )
+        .subcommand(
+            App::new("history")
+                .about("Get the history for the current week")
+        )
+        .subcommand(
+            App::new("track")
+                .about("Track worked hours for projects")
+                .setting(AppSettings::ArgRequiredElseHelp)
+                .arg(
+                    Arg::new("hours")
+                        .short('h')
+                        .long("hours")
+                        .about("Number of hours to track")
                         .takes_value(true)
                         .multiple(true),
                 ),
         )
         .get_matches();
 
-    // You can see which subcommand was used
-    if let Some(subcommand) = matches.subcommand_name() {
-        println!("'git {}' was used", subcommand);
+    match matches.subcommand() {
+        ("projects", Some(projects_matches)) => {
+            let all = projects_matches.is_present("all");
+            println!("Cloning {}", all);
 
-        // It's important to note, this *only* check's git's DIRECT children, **NOT** it's
-        // grandchildren, great grandchildren, etc.
-        //
-        // i.e. if the command `git push remove --stuff foo` was run, the above will only print out,
-        // `git push` was used. We'd need to get push's matches to see further into the tree
+            if all {
+                println!("get_projects is not implemented yet...")
+            } else {
+                println!("get_all_projects is not implemented yet...")
+            }
+        }
+        ("history", _) => {
+            println!("History is not implemented yet...")
+        }
+        ("track", Some(track_matches)) => {
+            // Now we have a reference to add's matches
+            println!(
+                "Tracking {}",
+                track_matches
+                    .values_of("stuff")
+                    .unwrap()
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            );
+        }
+        (_, None) => println!("No subcommand was used"), // If no subcommand was used it'll match the tuple ("", None)
+        _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
     }
 
     // An alternative to checking the name is matching on known names. Again notice that only the
