a86eb4be89df3b9712b42967ef69f364fac40e37
Even Kallevik <even.kallevik@gmail.com>
2020-09-11 16:31:43 +0200

    Remove dead code

--- src/http_client/mod.rs
+++ src/http_client/mod.rs
@@ -6,16 +6,6 @@ pub struct HTTPClient {
 
  impl HTTPClient {
 
-    #[warn(dead_code)]
-    pub async fn get_async<T : DeserializeOwned>(&self) -> Result<T, Box<dyn std::error::Error>> {
-        let resp = reqwest::get("https://httpbin.org/ip")
-            .await?
-            .json::<T>()
-            .await?;
-        Ok(resp)
-    }
-
-
      pub fn get<T : DeserializeOwned>(&self) -> Result<T, Box<dyn std::error::Error>> {
          let resp = reqwest::blocking::get("https://httpbin.org/ip")?
              .json::<T>()?;
