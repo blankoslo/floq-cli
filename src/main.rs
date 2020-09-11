356a6c71b9175b30595ff7a5f9b4cad1a9073634
emil daijiro <eg@blank.no>
2020-09-11 11:51:00 +0200

    http_client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod http_client;
+
+use std::env;
+use http_client::HTTPClient;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct IP {
+    origin: String
+}
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    println!("{}", args[1]);
+    println!("{}", args[2]);
+
+    let http_client = HTTPClient {
+        authorization: "hello".to_string()
+    };
+    let ip: IP = http_client.get()?;
+    println!("{:#?}", ip);
+    Ok(())
 }
