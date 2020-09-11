5526a4d8fd80416cbae942a57dfc876c5042e32b
Even Kallevik <even.kallevik@gmail.com>
2020-09-11 20:04:31 +0200

    Add basic commands

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,3 @@
-// Working with subcommands is simple. There are a few key points to remember when working with
-// subcommands in clap. First, they are really just Apps. This means they can have their own
-// settings, version, authors, args, and even their own subcommands. The next thing to remember is
-// that subcommands are set up in a tree like hierarchy.
-//
-// An ASCII art depiction may help explain this better. Using a fictional version of git as the demo
-// subject. Imagine the following are all subcommands of git (note, the author is aware these aren't
-// actually all subcommands in the real git interface, but it makes explanation easier)
-//
-//            Top Level App (git)                         TOP
-//                           |
-//    -----------------------------------------
-//   /             |                \          \
-// clone          push              add       commit      LEVEL 1
-//   |           /    \            /    \       |
-//  url      origin   remote    ref    name   message     LEVEL 2
-//           /                  /\
-//        path            remote  local                   LEVEL 3
-//
-// Given the above fictional subcommand hierarchy, valid runtime uses would be (not an all inclusive
-// list):
-//
-// $ git clone url
-// $ git push origin path
-// $ git add ref local
-// $ git commit message
-//
-// Notice only one command per "level" may be used. You could not, for example, do:
-//
-// $ git clone url push origin path
-//
-// It's also important to know that subcommands each have their own set of matches and may have args
-// with the same name as other subcommands in a different part of the tree hierarchy (i.e. the arg
-// names aren't in a flat namespace).
-//
-// In order to use subcommands in clap, you only need to know which subcommand you're at in your
-// tree, and which args are defined on that subcommand.
-//
-// Let's make a quick program to illustrate. We'll be using the same example as above but for
-// brevity sake we won't implement all of the subcommands, only a few.
-
 use clap::{App, AppSettings, Arg};
 use http_client::timetrack::HTTPClient;
 use serde::Deserialize;
@@ -137,28 +96,24 @@ fn main() {
     // The most common way to handle subcommands is via a combined approach using
     // `ArgMatches::subcommand` which returns a tuple of both the name and matches
     match matches.subcommand() {
-        ("clone", Some(clone_matches)) => {
-            // Now we have a reference to clone's matches
-            println!("Cloning {}", clone_matches.value_of("repo").unwrap());
-        }
-        ("push", Some(push_matches)) => {
-            // Now we have a reference to push's matches
-            match push_matches.subcommand() {
-                ("remote", Some(remote_matches)) => {
-                    // Now we have a reference to remote's matches
-                    println!("Pushing to {}", remote_matches.value_of("repo").unwrap());
-                }
-                ("local", _) => {
-                    println!("'git push local' was used");
-                }
-                _ => unreachable!(),
+        ("projects", Some(projects_matches)) => {
+            let all = projects_matches.is_present("all");
+            println!("Cloning {}", all);
+
+            if all {
+                println!("get_projects is not implemented yet...")
+            } else {
+                println!("get_all_projects is not implemented yet...")
             }
         }
-        ("add", Some(add_matches)) => {
+        ("history", _) => {
+            println!("History is not implemented yet...")
+        }
+        ("track", Some(track_matches)) => {
             // Now we have a reference to add's matches
             println!(
-                "Adding {}",
-                add_matches
+                "Tracking {}",
+                track_matches
                     .values_of("stuff")
                     .unwrap()
                     .collect::<Vec<_>>()
@@ -168,8 +123,6 @@ fn main() {
         (_, None) => println!("No subcommand was used"), // If no subcommand was used it'll match the tuple ("", None)
         _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
     }
-
-    // Continued program logic goes here...
 }
 
 fn get_env_var(key: &str) -> String {
