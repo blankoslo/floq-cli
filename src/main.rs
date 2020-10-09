c15904598bee2dcfe46bb3c054ca9fadd429070d
Even Kallevik <even.kallevik@gmail.com>
2020-10-09 10:36:58 +0200

    Fix merge issues

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
+mod http_client;
+
 use clap::{App, AppSettings, Arg};
+use http_client::timetrack::HTTPClient;
+use serde::Deserialize;
+use std::env;
+use dotenv::dotenv;
+
+#[derive(Deserialize, Debug)]
+struct IP {
+    origin: String
+}
+fn get_envvar(key: &str) -> String {
+    let envvar = env::var(key);
+    if envvar.is_err() {
+        panic!("env var {} not defined", key)
+    }
+    return envvar.unwrap()
+}
+
+fn setup() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let bearer_token = get_envvar("BEARER_TOKEN");
+    let http_client = HTTPClient::new(bearer_token);
+    let projects = http_client.get_current_week_timetracks(77)?;
+    println!("{:#?}", projects);
+    Ok(())
+}
 
 fn main() {
     let matches = App::new("timetracker")
