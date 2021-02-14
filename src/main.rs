382914829d584cd8259341719469580c198add54
Even Kallevik <even.kallevik@gmail.com>
2021-02-14 14:29:40 +0100

    Implement get_projects (#18)
    * Prettyprint projects
    
    * Remove pub(crate)
    
    * Refactorings
    
    * Fix write-lint
    
    * Nå kompilerer det
    
    * Fix clippy complaints and other minor stuff
    
    Authored-by: even.kallevik@blank.no
    Co-authored-by: emil daijiro <eg@blank.no>
    Co-authored-by: Trond Øydna <trond.oydna@blank.no>

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,19 @@ mod http_client;
 
 use chrono::{Duration, NaiveDate};
 use dotenv::dotenv;
-use std::env;
+use std::{env, fmt};
 
-use clap::{App, AppSettings, Arg};
+use crate::http_client::projects::Project;
+use clap::{App, AppSettings, Arg, ArgMatches};
 use http_client::HTTPClient;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error> > {
     let matches = App::new("timetracker")
         .about("Timetracking in the terminal")
         .version("1.0")
         .author("The Rust Gang")
+        .subcommand(App::new("demo").about("Get a demo of features not used elsewhere"))
         .subcommand(
             App::new("projects")
                 .about("Lists name and code of projects")
@@ -38,18 +41,32 @@ fn main() {
         )
         .get_matches();
 
+    let client = new_client();
+
+    async_std::task::block_on(async {
+        match perform_command(matches, client).await {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e)
+        }
+    })
+}
+
+async fn perform_command(matches: ArgMatches, client: HTTPClient) -> Result<(), Box<dyn Error>> {
     match matches.subcommand() {
+        Some(("demo", _)) => demo(client).await,
         Some(("projects", projects_matches)) => {
             let all = projects_matches.is_present("all");
 
             if all {
-                let client = new_client();
-                async_std::task::block_on(demo(client)).expect("Done");
+                print_all_projects(client).await
             } else {
-                println!("get_projects is not implemented yet...");
+                print_relevant_projects(client).await
             }
         }
-        Some(("history", _)) => println!("History is not implemented yet..."),
+        Some(("history", _)) => {
+            println!("History is not implemented yet...");
+            Ok(())
+        }
         Some(("track", track_matches)) => {
             let project_code = track_matches.value_of("project").unwrap();
             let hours = track_matches
@@ -59,10 +76,16 @@ fn main() {
                 .join(", ");
 
             println!("Test {} {}", project_code, hours);
+            Ok(())
         }
 
-        Some((_, _)) => unreachable!("Unknown commands should be handled by the library"),
-        None => println!("No subcommand was used"), // If all subcommands are defined above, anything else is unreachable!()
+        Some((_, _)) => {
+            unreachable!("Unknown commands should be handled by the library");
+        }
+        None => {
+            println!("No subcommand was used");
+            Ok(())
+        } // If all subcommands are defined above, anything else is unreachable!()
     }
 }
 
@@ -74,18 +97,7 @@ fn new_client() -> HTTPClient {
     HTTPClient::new(bearer_token, employee_id)
 }
 
-async fn demo(http_client: HTTPClient) -> Result<(), Box<dyn std::error::Error>> {
-    let projects = http_client.get_projects().await?;
-    println!("Projects:");
-    println!("{:#?}", projects);
-
-    let relevant_projects = http_client
-        .get_current_timetracked_projects_for_employee()
-        .await?;
-    println!();
-    println!("Relevant projects:");
-    println!("{:#?}", relevant_projects);
-
+async fn demo(http_client: HTTPClient) -> Result<(), Box<dyn Error>> {
     let current_week_timetrackings = http_client.get_current_week_timetracking().await?;
     println!();
     println!("Current week timetracking:");
@@ -100,9 +112,66 @@ async fn demo(http_client: HTTPClient) -> Result<(), Box<dyn std::error::Error>>
         .await?;
 
     println!("Done timetracking!");
+
+    Ok(())
+}
+
+async fn print_all_projects(http_client: HTTPClient) -> Result<(), Box<dyn std::error::Error>> {
+    let ui_projects = UIProjects(http_client.get_projects().await?);
+    println!("{}", ui_projects);
+    Ok(())
+}
+
+async fn print_relevant_projects(
+    http_client: HTTPClient,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let relevant_projects = UIProjects(
+        http_client
+            .get_current_timetracked_projects_for_employee()
+            .await?
+    );
+    println!("{}", relevant_projects);
     Ok(())
 }
 
 fn get_env_var(key: &str) -> String {
     env::var(key).unwrap_or_else(|_| panic!("env var {} not defined", key))
+}
+struct UIProjects(Vec<Project>);
+
+impl fmt::Display for UIProjects {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let headings = ["PROSJEKT KODE", "KUNDE", "BESKRIVELSE"];
+
+        let padding = 7;
+        let id_width = headings[0].len() + padding;
+        let customer_name_length = self.0.iter()
+            .map(|p| p.customer.name.len())
+            .max()
+            .unwrap_or(0);
+        let customer_width = customer_name_length + padding;
+
+        writeln!(
+                f,
+               "{:id_width$} {:customer_width$} {}",
+               headings[0],
+               headings[1],
+               headings[2],
+               id_width = id_width,
+               customer_width = customer_width
+        )?;
+
+        for project in self.0.iter() {
+            writeln!(
+                f,
+                "{:id_width$} {:customer_width$} {}",
+                project.id,
+                project.customer.name,
+                project.name,
+                id_width = id_width,
+                customer_width = customer_width
+            )?;
+        }
+        writeln!(f)
+    }
 }
