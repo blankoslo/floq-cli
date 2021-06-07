34a1da00b9b6b9cb6138c3328f3093a691efdba0
Erlend Hamberg <erlend@hamberg.no>
2021-06-07 12:54:39 +0200

    Slightly tweak help message

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -18,7 +18,7 @@ pub fn subcommand_app<'help>() -> App<'help> {
         .setting(AppSettings::ArgRequiredElseHelp)
         .subcommand(App::new("logg-inn").about("Logg inn i Floq"))
         .subcommand(
-            App::new("logg-ut").about("Logg ut av Floq, sletter din lokale brukerkonfigurasjon"),
+            App::new("logg-ut").about("Logg ut av Floq (sletter din lokale brukerkonfigurasjon)"),
         )
 }
 
